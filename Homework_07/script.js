9caf7e350087aa158bf529ea6615e3ee5ac62f09
AlexandruTanasoiu <alexandru.georgian15@gmail.com>
2023-05-21 23:42:44 +0300

    final updates

--- Homework_07/script.js
+++ Homework_07/script.js
@@ -1,14 +1,12 @@
 const path = "./data.json";
-let ok = 0;
 
-function parseFile(data) {}
 function pushJsonData(data, freq) {
   let i = 0;
   while (i < data.length) {
     console.log(i);
-    let idFirst = "current_" + data[i].title;
+    let idFirst = `current_${data[i].title}`;
     console.log(idFirst);
-    let idLast = "last_" + data[i].title;
+    let idLast = `last_${data[i].title}`;
     console.log(idLast);
     document.getElementById(idFirst).innerHTML =
       data[i].timeframes[freq].current;
@@ -18,14 +16,25 @@ function pushJsonData(data, freq) {
   }
 }
 
-function main_function() {
+function daily() {
+  fetch(path)
+    .then((response) => response.json())
+    .then((data) => pushJsonData(data, "daily"));
+}
+
+function weekly() {
+  fetch(path)
+    .then((response) => response.json())
+    .then((data) => pushJsonData(data, "weekly"));
+}
+
+function monthly() {
   fetch(path)
     .then((response) => response.json())
     .then((data) => pushJsonData(data, "monthly"));
 }
 
-document.getElementById("daily").addEventListener("click", main_function);
+document.getElementById("daily").addEventListener("click", daily);
+document.getElementById("weekly").addEventListener("click", weekly);
+document.getElementById("monthly").addEventListener("click", monthly);
 
-// document
-// .getElementById("daily")
-// .addEventListener("click", main_function());
