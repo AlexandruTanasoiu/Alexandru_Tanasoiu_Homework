0047c39dd2ea16d6976a751b8bfeb004152d6ed6
AlexandruTanasoiu <alexandru.georgian15@gmail.com>
2023-05-25 19:05:27 +0300

    button_style_updates

--- Homework_07/script.js
+++ Homework_07/script.js
@@ -13,17 +13,16 @@ function pushJsonData(data, freq) {
     document.getElementById(idLast).innerHTML =
       data[i].timeframes[freq].previous;
     i++;
-  }   
+  }
 }
 
-function button__style(id, state){
+function button__style(id, state) {
   if (state) {
-  document.getElementById(id).style.fontWeight = '700';
-  document.getElementById(id).style.color = 'white';
-  }
-  else {
-  document.getElementById(id).style.fontWeight = null;
-  document.getElementById(id).style.color = null;
+    document.getElementById(id).style.fontWeight = "700";
+    document.getElementById(id).style.color = "white";
+  } else {
+    document.getElementById(id).style.fontWeight = null;
+    document.getElementById(id).style.color = null;
   }
 }
 
@@ -31,32 +30,29 @@ function daily() {
   fetch(path)
     .then((response) => response.json())
     .then((data) => pushJsonData(data, "daily"));
-    button__style('daily', 1);
-    button__style('weekly', 0);
-    button__style('monthly', 0);
-  
- }
+  button__style("daily", 1);
+  button__style("weekly", 0);
+  button__style("monthly", 0);
+}
 
 function weekly() {
   fetch(path)
     .then((response) => response.json())
     .then((data) => pushJsonData(data, "weekly"));
-    button__style('daily', 0);
-    button__style('weekly', 1);
-    button__style('monthly', 0);
+  button__style("daily", 0);
+  button__style("weekly", 1);
+  button__style("monthly", 0);
 }
 
 function monthly() {
   fetch(path)
     .then((response) => response.json())
     .then((data) => pushJsonData(data, "monthly"));
-    button__style('daily', 0);
-    button__style('weekly', 0);
-    button__style('monthly', 1);
-    
+  button__style("daily", 0);
+  button__style("weekly", 0);
+  button__style("monthly", 1);
 }
 
 document.getElementById("daily").addEventListener("click", daily);
 document.getElementById("weekly").addEventListener("click", weekly);
 document.getElementById("monthly").addEventListener("click", monthly);
-
